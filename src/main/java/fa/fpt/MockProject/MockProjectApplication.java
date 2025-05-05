7962f3f0837142f9aba622928c8d537d48b91558
Ngô Minh <ngominhnhat125@gmail.com>
2025-05-05 12:30:15 +0700

    Update nội dung cho file pom

--- src/main/java/fa/fpt/MockProject/MockProjectApplication.java
+++ src/main/java/fa/fpt/MockProject/MockProjectApplication.java
@@ -2,9 +2,10 @@ package fa.fpt.MockProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MockProjectApplication {
+public class MockProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MockProjectApplication.class, args);
